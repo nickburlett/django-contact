7af25d607a8b7cfad6a4511e17a15b6ee7e8c045
Brandon R. Stoner <monokrome@monokro.me>
2010-07-28 17:11:27 -0600

    Initial project import

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name='Django Contacts',
+      version='0.1',
+      description='System for working with contacts/identities in django.',
+      author='Brandon R. Stoner',
+      author_email='monokrome@monokro.me',
+      url='http://monokro.me/',
+      packages=['django.contrib.contacts'],
+    )
+
