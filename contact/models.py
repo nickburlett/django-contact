8f136cbaeaf66c48b931419f8f268126adbc9534
Brandon R. Stoner <monokrome@LimpidTech.com>
2012-03-13 10:43:07 -0600

    Updated models to be more PEP8 aware.

--- contact/models.py
+++ contact/models.py
@@ -9,7 +9,10 @@ class LocationType(models.Model):
     """ Represents a type of location that specific contact information
         might be related to. For instance, someone might have both an
         "office" and a "home" phone number. This represents two location
-        types, "office" and "home". """
+        types, "office" and "home".
+
+    """
+
     name = models.CharField(max_length=16)
 
     def __unicode__(self):
@@ -17,6 +20,7 @@ class LocationType(models.Model):
 
 class WebSite(models.Model):
     """ A website that is affiliate with this contact """
+
     name = models.CharField(max_length=64, null=True, blank=True)
     url = models.URLField(max_length=128)
     location = models.ForeignKey(LocationType, related_name='websites', null=True, blank=True)
@@ -29,6 +33,7 @@ class WebSite(models.Model):
 
 class Phone(models.Model):
     """ Represents a phone. This phone has a number and a location. """
+
     number = models.CharField(max_length=17) # TODO: Use localflavor for phone numbers?
     location = models.ForeignKey(LocationType, related_name='phones', null=True, blank=True)
 
@@ -37,7 +42,10 @@ class Phone(models.Model):
 
 class Address(models.Model):
     """ Represents a location on Earth. My apologies in the case that you
-        live on another planet. """
+        live on another planet.
+
+    """
+
     class Meta(object):
         verbose_name_plural = 'addresses'
 
@@ -48,6 +56,7 @@ class Address(models.Model):
 
 class Email(models.Model):
     """ Represents an email's address and the location data relative to it. """
+
     address = models.EmailField()
     location = models.ForeignKey(LocationType, related_name='emails', null=True, blank=True)
 
@@ -56,6 +65,7 @@ class Email(models.Model):
 
 class Date(models.Model):
     """ In case someone wants to remember an important date, we provide a date model. """
+
     name = models.CharField(max_length=64)
     date = models.DateTimeField()
 
@@ -65,7 +75,10 @@ class Date(models.Model):
 class CustomData(models.Model):
     """ This is a simple type of field data that can be linked to an identity
         so that "unsupported" types of data are able to be linked to your
-        contacts if ever required. """
+        contacts if ever required.
+
+    """
+
     class Meta(object):
         verbose_name_plural = 'custom data'
 
@@ -78,10 +91,11 @@ class CustomData(models.Model):
 
 class IdentityData(models.Model):
     """ Arbitrary data that can be linked to a contact. """
+
     class Meta(object):
         verbose_name_plural = 'identity data'
 
-    """ This is used to wrap our ManyToManyField to a generic relation """
+    # This is used to wrap our ManyToManyField to a generic relation
     content_type = models.ForeignKey(ContentType)
     object_id = models.PositiveIntegerField()
     content_object = generic.GenericForeignKey()
@@ -124,6 +138,7 @@ class Identity(models.Model):
 
 class Person(Identity):
     """ An identity that is a person, or possibly is masquerading as one! """
+
     class Meta(object):
         verbose_name_plural = 'people'
 
@@ -139,6 +154,7 @@ class Person(Identity):
 
 class Company(Identity):
     """ An organization or other entity that has an identity but is not a person. """
+
     class Meta(object):
         verbose_name_plural = 'companies'
 
@@ -149,6 +165,7 @@ class Company(Identity):
 
 class Group(models.Model):
     """ Allows identities to be added to different groups in the system. """
+
     name = models.CharField(max_length=32)
     members = models.ManyToManyField(Identity, blank=True)
 
