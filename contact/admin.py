44b148a990fa52e764e98681b87a5b5f570ebc12
Brandon R. Stoner <monokrome@limpidtech.com>
2013-06-05 11:58:21 -0700

    Use relative imports.

--- contact/admin.py
+++ contact/admin.py
@@ -1,15 +1,15 @@
 from django.contrib import admin
 
-from models import LocationType
-from models import Phone
-from models import Address
-from models import Email
-from models import Person
-from models import Company
-from models import WebSite
-from models import Group
-from models import CustomData
-from models import IdentityData
+from .models import LocationType
+from .models import Phone
+from .models import Address
+from .models import Email
+from .models import Person
+from .models import Company
+from .models import WebSite
+from .models import Group
+from .models import CustomData
+from .models import IdentityData
 
 admin.site.register(LocationType)
 admin.site.register(Phone)
